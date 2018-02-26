5b9edc55806ae15765000da453270f5efc1a358f
CarlosW4E <carlorpri@gmail.com>
2018-02-26 12:17:36 +0100

    arreglo en asnwer repository

--- D08/Acme-Rendezvous/src/main/java/repositories/AnswerRepository.java
+++ D08/Acme-Rendezvous/src/main/java/repositories/AnswerRepository.java
@@ -15,7 +15,7 @@ public interface AnswerRepository extends JpaRepository<Answer, Integer> {
 	@Query("select a from Answer a where a.rsvp.id=?1 and a.question.id=?2")
 	Answer findByRSVPIdAndQuestionId(int RSVPId, int questionId);
 
-	@Query("select a from Answer a where a.question.rendezvous.id=?1 and a.question.rendezvous.creator.id=?2")
+	@Query("select count(a) from Answer a where a.question.rendezvous.id=?1 and a.question.rendezvous.creator.id=?2")
 	Integer countRendezvousIdAndUserId(int rendezvousId, int userId);
 
 	@Query("select a from Answer a where a.question.id=?1 and a.rsvp.attendant.id=?2")
