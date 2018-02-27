f37a103aba29eadf619f5c6c73631b5a00eaadeb
CarlosW4E <carlorpri@gmail.com>
2018-02-27 03:35:23 +0100

    Cambios en announcement

--- D08/Acme-Rendezvous/src/main/java/repositories/AnnouncementRepository.java
+++ D08/Acme-Rendezvous/src/main/java/repositories/AnnouncementRepository.java
@@ -1,3 +1,4 @@
+
 package repositories;
 
 import org.springframework.data.domain.Page;
@@ -11,7 +12,7 @@ import domain.Announcement;
 @Repository
 public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
 
-	@Query("select a from Announcement a where a.rendezvous.id = ?1")
+	@Query("select a from Announcement a where a.rendezvous.id = ?1 order by a.rendezvous.moment DESC")
 	Page<Announcement> findByRendezvousId(final int rendezvousId, final Pageable pageable);
 
 	@Query("select count(a) from Announcement a where a.rendezvous.id = ?1")
@@ -19,10 +20,10 @@ public interface AnnouncementRepository extends JpaRepository<Announcement, Inte
 
 	@Query("select a from Announcement a where a.rendezvous.creator.userAccount.id = ?1")
 	Page<Announcement> findByCreatorUserAccountId(final int userAccountId, final Pageable pageable);
-	
+
 	@Query("select count(a) from Announcement a where a.rendezvous.creator.userAccount.id = ?1")
 	Integer countByCreatorUserAccountId(final int rendezvousId);
-	
+
 	@Query("select avg(cast((select count(a) from Announcement a where a.rendezvous.id=r.id)as int)),sqrt(sum((select count(a) from Announcement a where a.rendezvous.id=r.id)*(select count(a) from Announcement a where a.rendezvous.id=r.id))/(select count(r2) from Rendezvous r2)-avg(cast((select count(a) from Announcement a where a.rendezvous.id=r.id) as float ))*avg(cast((select count(a) from Announcement a where a.rendezvous.id=r.id) as float ))) from Rendezvous r")
 	Double[] avgStandartDerivationAnnouncementPerRendezvous();
 
