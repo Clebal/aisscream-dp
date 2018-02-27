5c9dd65b4cd141e9b08b1a3deb381c2adb026a17
Sergio Clebal <sergioclebal@hotmail.com>
2018-02-27 13:49:19 +0100

    Arreglado AnnouncementService.

--- D08/Acme-Rendezvous/src/main/java/services/AnnouncementService.java
+++ D08/Acme-Rendezvous/src/main/java/services/AnnouncementService.java
@@ -1,5 +1,6 @@
 package services;
 
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 
@@ -67,11 +68,17 @@ public class AnnouncementService {
 	 
 	public Announcement save(final Announcement announcement) {
 		Announcement result, saved;
+		Calendar calendar;
 		
 		Assert.notNull(announcement);
 		Assert.isTrue(announcement.getRendezvous().getCreator().getUserAccount().equals(LoginService.getPrincipal()));
 		
-		if(announcement.getId() == 0) announcement.setMoment(new Date(System.currentTimeMillis() - 1));
+		if(announcement.getId() == 0){
+			calendar = Calendar.getInstance();
+			calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), 0);
+			calendar.getTime().setTime(calendar.getTimeInMillis() - 1);
+			announcement.setMoment(calendar.getTime());
+		}
 		
 		if(announcement.getId() != 0) {
 			saved = this.announcementRepository.findOne(announcement.getId());
