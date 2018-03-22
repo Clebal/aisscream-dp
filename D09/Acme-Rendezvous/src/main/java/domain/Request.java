bf7241155cca6b5d9346c42c106b8a549427d732
CarlosW4E <carlorpri@gmail.com>
2018-03-22 14:31:16 +0100

    Índices que nos faltaban

--- D09/Acme-Rendezvous/src/main/java/domain/Request.java
+++ D09/Acme-Rendezvous/src/main/java/domain/Request.java
@@ -5,16 +5,23 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(uniqueConstraints = {
+	@UniqueConstraint(columnNames = {
+		"service_id", "rendezvous_id"
+	})
+})
 public class Request extends DomainEntity {
 
 	private String		comments;
 
-	private Service	service;
+	private Service		service;
 
 	private Rendezvous	rendezvous;
 
--- D09/Acme-Rendezvous/src/main/java/domain/Service.java
+++ D09/Acme-Rendezvous/src/main/java/domain/Service.java
@@ -6,8 +6,10 @@ import java.util.Collection;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -18,6 +20,9 @@ import org.hibernate.validator.constraints.URL;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "status")
+})
 public class Service extends DomainEntity {
 
 	private String					name;
