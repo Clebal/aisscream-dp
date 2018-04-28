282786eb856c40822508c18f5d8b9aac43a58e82
CarlosW4E <carlorpri@gmail.com>
2018-04-28 13:43:34 +0200

    Atributo + hasTaboo : Boolean en Advertisement

--- D11/Acme-Newspaper/src/main/java/domain/Advertisement.java
+++ D11/Acme-Newspaper/src/main/java/domain/Advertisement.java
@@ -1,3 +1,4 @@
+
 package domain;
 
 import javax.persistence.Access;
@@ -14,64 +15,75 @@ import org.hibernate.validator.constraints.URL;
 @Access(AccessType.PROPERTY)
 public class Advertisement extends DomainEntity {
 
-	private String title;
-	
-	private String urlBanner;
-	
-	private String urlTarget;
-	
-	private CreditCard creditCard;
-	
-	private Agent agent;
-		
+	private String		title;
+
+	private String		urlBanner;
+
+	private String		urlTarget;
+
+	private CreditCard	creditCard;
+
+	private Agent		agent;
+
+	private boolean		hasTaboo;
+
+
 	@NotBlank
 	public String getTitle() {
-		return title;
+		return this.title;
 	}
 
-	public void setTitle(String title) {
+	public void setTitle(final String title) {
 		this.title = title;
 	}
 
 	@NotBlank
 	@URL
 	public String getUrlBanner() {
-		return urlBanner;
+		return this.urlBanner;
 	}
 
-	public void setUrlBanner(String urlBanner) {
+	public void setUrlBanner(final String urlBanner) {
 		this.urlBanner = urlBanner;
 	}
 
 	@NotBlank
 	@URL
 	public String getUrlTarget() {
-		return urlTarget;
+		return this.urlTarget;
 	}
 
-	public void setUrlTarget(String urlTarget) {
+	public void setUrlTarget(final String urlTarget) {
 		this.urlTarget = urlTarget;
 	}
 
 	@NotNull
 	@Valid
 	public CreditCard getCreditCard() {
-		return creditCard;
+		return this.creditCard;
 	}
 
-	public void setCreditCard(CreditCard creditCard) {
+	public void setCreditCard(final CreditCard creditCard) {
 		this.creditCard = creditCard;
 	}
-	
+
 	@NotNull
 	@Valid
 	@ManyToOne(optional = false)
 	public Agent getAgent() {
 		return this.agent;
 	}
-	
+
 	public void setAgent(final Agent agent) {
 		this.agent = agent;
 	}
-	
+
+	public boolean getHasTaboo() {
+		return this.hasTaboo;
+	}
+
+	public void setHasTaboo(final boolean hasTaboo) {
+		this.hasTaboo = hasTaboo;
+	}
+
 }
