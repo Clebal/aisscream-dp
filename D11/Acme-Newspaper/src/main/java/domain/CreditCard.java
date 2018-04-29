9c2eb404a2f6c20d6f0675d280cc657879e9e4d2
Sergio Clebal <sergioclebal@gmail.com>
2018-04-30 01:43:25 +0200

    Añadida reestricción en el expirationYear de CreditCard.

--- D11/Acme-Newspaper/src/main/java/domain/CreditCard.java
+++ D11/Acme-Newspaper/src/main/java/domain/CreditCard.java
@@ -1,31 +1,34 @@
-
 package domain;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
-import javax.persistence.Embeddable;
-import javax.validation.constraints.Min;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.Range;
 
-@Embeddable
+@Entity
 @Access(AccessType.PROPERTY)
-public class CreditCard {
+public class CreditCard extends DomainEntity {
 
-	private String		holderName;
+	private String holderName;
 
-	private String		brandName;
+	private String brandName;
 
-	private String		number;
+	private String number;
 
-	private int			expirationMonth;
+	private int expirationMonth;
 
-	private int			expirationYear;
+	private int expirationYear;
 
-	private int			cvvcode;
+	private int cvvcode;
 	
+	private User user;
+
 	@NotBlank
 	public String getHolderName() {
 		return this.holderName;
@@ -63,7 +66,7 @@ public class CreditCard {
 		this.expirationMonth = expirationMonth;
 	}
 
-	@Min(0)
+	@Range(min = 0, max = 99)
 	public int getExpirationYear() {
 		return this.expirationYear;
 	}
@@ -79,6 +82,17 @@ public class CreditCard {
 
 	public void setCvvcode(final int cvvcode) {
 		this.cvvcode = cvvcode;
+	}
+	
+	@Valid
+	@NotNull
+	@ManyToOne(optional = false)
+	public User getUser() {
+		return this.user;
+	}
+	
+	public void setUser(final User user) {
+		this.user = user;
 	}
 
 }
