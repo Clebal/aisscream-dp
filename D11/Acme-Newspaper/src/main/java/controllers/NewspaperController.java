41cb93e1a0de40dad37003e7413eb412c22b1e30
Santosale <alesanmon@alum.us.es>
2018-04-30 16:09:23 +0200

    Fallo arreglado de ver los privados

--- D11/Acme-Newspaper/src/main/java/controllers/NewspaperController.java
+++ D11/Acme-Newspaper/src/main/java/controllers/NewspaperController.java
@@ -183,10 +183,11 @@ public class NewspaperController extends AbstractController {
 		authority.setAuthority("CUSTOMER");
 		if (newspaper.getIsPrivate() == false)
 			canSeeArticles = true;
-		else if (LoginService.isAuthenticated()
-			&& LoginService.getPrincipal().getAuthorities().contains(authority)
-			&& (this.subscriptionNewspaperService.findByCustomerAndNewspaperId(this.customerService.findByUserAccountId(LoginService.getPrincipal().getId()).getId(), newspaper.getId()) != null || !this.volumeService.findByCustomerIdAndNewspaperId(
-				this.customerService.findByUserAccountId(LoginService.getPrincipal().getId()).getId(), newspaper.getId()).isEmpty()))
+		else if (LoginService.isAuthenticated() && LoginService.getPrincipal().getAuthorities().contains(authority)
+			&& (this.subscriptionNewspaperService.findByCustomerAndNewspaperId(this.customerService.findByUserAccountId(LoginService.getPrincipal().getId()).getId(), newspaper.getId()).size() == 1))
+			canSeeArticles = true;
+		else if (LoginService.isAuthenticated() && LoginService.getPrincipal().getAuthorities().contains(authority)
+			&& this.volumeService.findByCustomerIdAndNewspaperId(this.customerService.findByUserAccountId(LoginService.getPrincipal().getId()).getId(), newspaper.getId()).size() > 0)
 			canSeeArticles = true;
 		else if (LoginService.isAuthenticated() && newspaper.getPublisher().getUserAccount().getId() == LoginService.getPrincipal().getId())
 			canSeeArticles = true;
