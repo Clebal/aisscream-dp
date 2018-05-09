edcafdb279d712340c1f61298bbf800b64bcb283
Alejandro Sánchez Villarín <alesanvil3@alum.us.es>
2018-05-09 23:18:18 +0200

    Delete acme newspaper

---
+++ D11 - Acceptance testing/Item 7/delete-acme-newspaper.sql
@@ -0,0 +1,15 @@
+start transaction;
+
+use `Acme-Newspaper`;
+
+revoke all privileges on `Acme-Newspaper`.* from 'acme-user'@'%';
+
+revoke all privileges on `Acme-Newspaper`.* from 'acme-manager'@'%';
+
+drop user 'acme-user'@'%';
+
+drop user 'acme-manager'@'%';
+
+drop database if exists `Acme-Newspaper`;
+
+commit;
