c9e420d6d0e4e222d379ecb41977c96ad4d82919
Alejandro Sánchez Villarín <alesanvil3@alum.us.es>
2018-05-09 21:51:35 +0200

    Quitados system.out

--- D11/Acme-Newspaper/src/test/java/usecases/SaveSubscriptionNewspaperTest.java
+++ D11/Acme-Newspaper/src/test/java/usecases/SaveSubscriptionNewspaperTest.java
@@ -137,7 +137,6 @@ public class SaveSubscriptionNewspaperTest extends AbstractTest {
 		
 		for (int i = 0; i < testingData.length; i++)
 			try {
-				System.out.println(i);
 				super.startTransaction();
 				this.template((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (int) testingData[i][4], (int) testingData[i][5], (int) testingData[i][6], (String) testingData[i][7],  (String) testingData[i][8], (Class<?>) testingData[i][9]);
 			} catch (final Throwable oops) {
@@ -205,8 +204,6 @@ public class SaveSubscriptionNewspaperTest extends AbstractTest {
 		} catch (final Throwable oops) {
 			caught = oops.getClass();
 		}
-		System.out.println("Expected: " + expected);
-		System.out.println("Caught: " + caught);
 
 		super.checkExceptions(expected, caught);
 	}
