1b013fd97061ba48dba7a113257f867196a9fad5
Sergio Clebal <sergioclebal@gmail.com>
2018-05-22 10:38:55 +0200

    Quitado warning.

--- Hackaton/Acme-Chollos-Rifas/src/main/java/controllers/SurveyController.java
+++ Hackaton/Acme-Chollos-Rifas/src/main/java/controllers/SurveyController.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.DataBinder;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
